291dfd2209c9c818a2032fa84156af7a7db0a235
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2026-04-02 22:51:20 +0200

    Update and optimize the GUIDFinder

--- Editor/Tools/GuidFinder.cs
+++ Editor/Tools/GuidFinder.cs
@@ -5,93 +5,60 @@ using UnityEditor;
 using UnityEngine;
 
 
-
-
-namespace PossumScream.Editor.Tools
+namespace DragonResonance.Editor.Tools
 {
 	public class GuidFinder : EditorWindow
 	{
-		private Object retrievedObject = null;
-		private bool assetFound = false;
-		private string inputGuid = "";
-		private string retrievedPath = "";
-
-
+		private bool _wasFound = false;
+		private Object _foundObject = null;
+		private string _foundPath = "";
+		private string _inputGuid = "";
 
 
 		#region Events
 
-
 			private void OnGUI()
 			{
 				EditorGUILayout.LabelField("Prompt", EditorStyles.boldLabel);
-
-				this.inputGuid = EditorGUILayout.TextField("GUID", this.inputGuid);
-
-				if (GUILayout.Button("Search in Asset Database")) {
-					this.assetFound = tryFindAsset(this.inputGuid, out this.retrievedPath, out this.retrievedObject);
-				}
-
-				if (GUILayout.Button("Clean field")) {
-					this.assetFound = false;
-					this.inputGuid = "";
-					this.retrievedObject = null;
-					this.retrievedPath = "";
-				}
-
+				_inputGuid = EditorGUILayout.TextField("GUID", _inputGuid);
+				if (GUILayout.Button("Find"))
+					_wasFound = TryFindAsset(_inputGuid, out _foundPath, out _foundObject);
 
 				EditorGUILayout.LabelField("Results", EditorStyles.boldLabel);
-
-				if (this.assetFound) {
+				if (_wasFound) {
 					EditorGUILayout.LabelField("Asset found!");
-					EditorGUILayout.TextField("Path", this.retrievedPath);
+					EditorGUILayout.TextField("Path", _foundPath);
 
-					if (GUILayout.Button("Highlight asset in Project")) {
-						EditorGUIUtility.PingObject(this.retrievedObject);
-					}
+					if (GUILayout.Button("Highlight asset in Project"))
+						EditorGUIUtility.PingObject(_foundObject);
 
-					if (GUILayout.Button("Select asset in Project")) {
-						Selection.objects = new[] { this.retrievedObject };
-					}
+					if (GUILayout.Button("Select asset in Project"))
+						Selection.objects = new[] { _foundObject };
 				}
 				else {
 					EditorGUILayout.LabelField("Asset not found.");
 				}
 			}
 
+		#endregion
+
+
+		#region Publics
+
+			[MenuItem("Window/Dragon Resonance/GUID Finder")]
+			public static void CreateWindow() => GetWindow<GuidFinder>("GUID Finder");
 
 		#endregion
 
 
+		#region Privates
 
-
-		#region Controls
-
-
-			[MenuItem("Window/PossumScream/GUID Finder")]
-			public static void CreateWindow()
-			{
-				GetWindow<GuidFinder>("GUID Finder");
-			}
-
-
-		#endregion
-
-
-
-
-		#region Actions
-
-
-			private static bool tryFindAsset(string guid, out string path, out Object unityObject)
+			private static bool TryFindAsset(string guid, out string path, out Object unityObject)
 			{
 				path = AssetDatabase.GUIDToAssetPath(guid);
 				unityObject = AssetDatabase.LoadAssetAtPath<Object>(path);
-
-
-				return (path.Length > 0);
+				return !string.IsNullOrEmpty(path);
 			}
-
 
 		#endregion
 	}
