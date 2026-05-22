8e04299941631ea5d8cd6e5164b28e6194c41bb9
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2026-05-22 19:44:02 +0200

    Add IsFullyVisible and IsFullyVisible extension methods for CanvasGroup

---
+++ Runtime/Extensions/CanvasGroupExtensions.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+
+namespace DragonResonance.Extensions
+{
+	public static class CanvasGroupExtensions
+	{
+		public static bool IsFullyVisible(this CanvasGroup target) => target.alpha.Approximately(1f);
+		public static bool IsFullyInvisible(this CanvasGroup target) => target.alpha.Approximately(0f);
+	}
+}
+
+
+/*       ________________________________________________________________       */
+/*           _________   _______ ________  _______  _______  ___    _           */
+/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
+/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
+/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
+/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
+/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
+/*       ________________________________________________________________       */
+/*                                                                              */
+/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
+/*           Dragon Resonance    <https://github.com/dragonresonance>           */
+/*                  Copyright © 2021-2026. All rights reserved.                 */
+/*                Licensed under the Apache License, Version 2.0.               */
+/*                         See LICENSE.md for more info.                        */
+/*       ________________________________________________________________       */
+/*                                                                              */
