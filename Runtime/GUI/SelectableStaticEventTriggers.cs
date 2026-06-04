86be08768ce7f94f8b2cdf611530335495d5439d
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2026-06-05 01:23:15 +0200

    Fix script copyright

--- Runtime/GUI/SelectableStaticEventTriggers.cs
+++ Runtime/GUI/SelectableStaticEventTriggers.cs
@@ -54,6 +54,7 @@ public class SelectableStaticEventTriggers : PossumBehaviour,
 
 	#endregion
 
+
 	#region Properties
 
 		public Selectable Selectable => (_selectable_internal = GetComponentIfNull(_selectable_internal));
@@ -62,7 +63,19 @@ public class SelectableStaticEventTriggers : PossumBehaviour,
 }
 
 
-/*                                                                                */
-/*        Windmill                                   Copyright © 2025-2026        */
-/*        Praenaris                                    All rights reserved        */
-/*                                                                                */
+/*       ________________________________________________________________       */
+/*           _________   _______ ________  _______  _______  ___    _           */
+/*           |        \ |______/ |______| |  _____ |       | |  \   |           */
+/*           |________/ |     \_ |      | |______| |_______| |   \__|           */
+/*           ______ _____ _____ _____ __   _ _____ __   _ _____ _____           */
+/*           |____/ |____ [___  |   | | \  | |___| | \  | |     |____           */
+/*           |    \ |____ ____] |___| |  \_| |   | |  \_| |____ |____           */
+/*       ________________________________________________________________       */
+/*                                                                              */
+/*           David Tabernero M.  <https://github.com/davidtabernerom>           */
+/*           Dragon Resonance    <https://github.com/dragonresonance>           */
+/*                  Copyright © 2021-2026. All rights reserved.                 */
+/*                Licensed under the Apache License, Version 2.0.               */
+/*                         See LICENSE.md for more info.                        */
+/*       ________________________________________________________________       */
+/*                                                                              */
